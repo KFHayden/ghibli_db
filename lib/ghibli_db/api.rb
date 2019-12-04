120edf605fab6786907e439a29e5c7afde2e153e
Kevin Hayden <kevin.fhayden@gmail.com>
2019-12-04 02:09:02 +0000

    Update API code

--- lib/ghibli_db/api.rb
+++ lib/ghibli_db/api.rb
@@ -1,10 +1,22 @@
 class GhibliDb::API
   #retrieve data here
   def fetch
-    url = "https://ghibliapi.herokuapp.com"
+    url = "https://ghibliapi.herokuapp.com/films"
     response = HTTParty.get(url)
-    response.parsed_response
+    response.each do |film|
+      title = film["title"]
+      description = film["description"]
+      director = film["director"]
+      producer = film["producer"]
+      release_date = film["release_date"]
+      rotten_tomato = film["rt_score"]
+      link_for_people = film["people"]
+      link_for_species = film["species"]
+      link_for_locations = film["locations"]
+      link_for_vehicles = film["vehicles"]
+    
     binding.pry
+  end
   end
   
 end
