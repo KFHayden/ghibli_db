232fa6578e90881c74395eef37985b66e4bbbe8e
Kevin Hayden <kevin.fhayden@gmail.com>
2019-12-04 21:03:23 +0000

    Iterated through titles with index, Notes

--- lib/ghibli_db/api.rb
+++ lib/ghibli_db/api.rb
@@ -2,22 +2,22 @@ class API
   #retrieve data here
   attr_accessor :ghibli
   
-  def self.fetch_films
+  def fetch_films
     url = "https://ghibliapi.herokuapp.com/films"
     response = HTTParty.get(url)
-    parse = response.parsed_response
-    parse.map do |parse| 
-      title = parse["title"]
-      description = parse["description"]
-      director = parse["director"]
-      producer = parse["producer"]
-      release_date = parse["release_date"]
-      rt_score = parse["rt_score"]
-      people = parse["people"]
-      species = parse["species"]
-      locations = parse["locations"]
+    response.parsed_response.each do |film| 
+      title = film["title"]
+      description = film["description"]
+      director = film["director"]
+      producer = film["producer"]
+      release_date = film["release_date"]
+      rt_score = film["rt_score"]
+      people = film["people"]
+      species = film["species"]
+      locations = film["locations"]
       
       Films.new(title, description, director, producer, release_date, rt_score, people, species, locations)
+
     end
   end
   
--- lib/ghibli_db/cli.rb
+++ lib/ghibli_db/cli.rb
@@ -2,27 +2,42 @@ class CLI
   
   def call
     #call menu here
-    puts "Would you like to view films?"
-    puts "y/n"
-    input = gets.strip
-    if input.downcase == "y" || input.downcase == "yes"
-      film_titles
-    elsif input.downcase == "n" || input.downcase == "no"
-      puts "Have a nice day!"
-    else
-      puts "I'm sorry, I didn't get that."
-      call
-    end
+    API.new.fetch_films
+    puts "Welcome to the wonderful world of Ghibli!"
+    menu
+    
   end
   
   def film_titles
     #iterate through movie objects here
     Films.all.each.with_index do |film, i|
-      
+      puts "#{i+1}. #{film.title}"
+    end
+    puts ""
+    puts "Which film would you like to know more about?"
+  end
+  
+  def film_info
+    #pull info from index related to title here
   end
   
   def menu
-    #loop here
+    puts "Would you like to view films?"
+    puts ""
+    puts "Please choose 'yes' or 'no'"
+    input = gets.strip
+    if input.downcase == "y" || input.downcase == "yes"
+      film_titles
+    elsif input.downcase == "n" || input.downcase == "no"
+      exit_program
+    else
+      puts "I'm sorry, I didn't get that."
+      menu
+    end
+  end
+  
+  def exit_program
+    puts "Have a nice day!"
   end
   
 end
